c2661a80a2da70d56c46a0a6a21d3f57a247ad97
jessicamh13 <jessica.mh13@yahoo.com>
2018-07-13 14:28:51 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -8,4 +8,27 @@ def eur_to_usd(dollar)
   dollar/1.2
 end
 
-def
+def usd_to_jpy(yen)
+  yen 
+end
+
+def jpy_to_usd(dollar)
+  dollar
+end
+
+def usd_to_gbp(pound)
+  pound
+end
+
+def gbp_to_usd(dollar)
+  dollar
+end
+
+def usd_to_aud(ausdol)
+  ausdol 
+end
+
+def aud_to_usd(dollar)
+  dollar
+end
+  
